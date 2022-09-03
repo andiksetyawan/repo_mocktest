50f323f274ddf00c6e84201bc93a5c3e5de138f6
Andik Setyawan <andik.setyawan19@gmail.com>
2022-09-04 03:47:40 +0700

    user domain

---
+++ domain/user.go
@@ -0,0 +1,18 @@
+package domain
+
+import "context"
+
+//User Domain
+type User struct {
+	ID      int64  `json:"id" bson:"id"`
+	Name    string `json:"name" bson:"name"`
+	Address string `json:"address" bson:"address"`
+}
+
+type UserRepository interface {
+	Save(ctx context.Context, user *User) error
+	UpdateByID(ctx context.Context, id string, user *User) error
+	FindAll(ctx context.Context, user *User) (*[]*User, error)
+	FindByID(ctx context.Context, user *User) (*User, error)
+	DeleteByID(ctx context.Context, id string) error
+}
